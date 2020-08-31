9f7b838b053e41114b0c70daf46ed893f2551798
ZIA <68376285+SnakesterZ@users.noreply.github.com>
2020-08-31 12:32:17 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,35 @@
+class Box{
+    constructor(x, y, width, height){
+        var option = {
+            restitution:0.3,
+            friction:1
+        }
+    this.body = Bodies.rectangle(x, y, width, height, option);
+        this.width = width;
+        this.height = height;
+        
+        World.add(world, this.body);
+      }
+      display(){
+        var speed = this.body.speed
+        if(speed<3){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill(255);
+        rect(0, 0, this.width, this.height);
+        pop();
+        }
+        else{
+          World.remove(world, this.body)
+          push();
+          
+          this.visibility = this.visibility-5
+          pop();
+        }
+      }
+      
+    }
